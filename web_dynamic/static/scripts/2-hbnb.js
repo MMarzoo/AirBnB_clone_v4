0bc3257fa5ba8e336ec8d82b0a1c5e33ab54c024
MMarzoo <marzouk12378@gmail.com>
2024-06-02 20:01:57 +0300

    TASK#3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  const checkedAmenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      checkedAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete checkedAmenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(checkedAmenities).join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
